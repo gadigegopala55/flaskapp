7d251038c6e2c7e7861042e55af7c4b6210120db
Gopala Krishna Gadige <gadigegopala55@gmail.com>
2022-07-13 16:55:32 +0530

    updating the database connection

--- flaskapp.py
+++ flaskapp.py
@@ -9,63 +9,56 @@ mail = Mail(app) # instantiate the mail class
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = "demomail224@gmail.com"
-app.config['MAIL_PASSWORD'] = "oxuitzgzzerxsgam"
+app.config['MAIL_PASSWORD'] = "hxmupdlctzszvlkt"
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
+connection = sqlite3.connect("post.db",check_same_thread=False)
+conn = connection.cursor()
 
 #creating a admin database
 @app.route("/create")
 def create():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("CREATE TABLE IF NOT EXISTS admin(id INTEGER PRIMARY KEY,userid VARCHAR(40),password VARCHAR(40),userlogin VARCHAR(40))")
         return "<h1>Successfull<h1>"
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #inserting a admin data(userid,password)
 @app.route("/insert")
 def insert():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute('INSERT INTO admin VALUES(1,"gopal","krishna","failure")')
         connection.commit()
         return "<h1>Data Entered successfully<h1>"
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #creating a post database
 @app.route("/createpost")
 def createpost():
     try:
-        connection = sqlite3.connect("post.db")
-        conn = connection.cursor()
         conn.execute("CREATE TABLE IF NOT EXISTS post(title VARCHAR(400),description VARCHAR(400))")
         return "<h1>Successfull<h1>"
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #Home Page
 @app.route("/")
 def home():
     try:
-        connection = sqlite3.connect("post.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM post")
         mydata = conn.fetchall()
         return render_template("home.html",mydata = mydata)
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #Login Page
 @app.route("/login",methods=["GET","POST"])
 def login():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
@@ -82,15 +75,13 @@ def login():
         else:
             return redirect("/post")
         return render_template("login.html",message="")
-    except:
-        return render_template("login.html",message = "Something Went Wrong")
+    except Exception as error:
+        return render_template("login.html",message = f'{error}')
 
 # Posting a blog
 @app.route("/post",methods=["GET","POST"])
 def post():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
@@ -102,12 +93,8 @@ def post():
                 description = request.form["description"]
                 if not title or not description:
                     return render_template("post.html",message="* Enter all fields")
-                connection = sqlite3.connect("post.db")
-                conn = connection.cursor()
                 conn.execute(f'INSERT INTO post VALUES("{title}","{description}")')
                 connection.commit()
-                connection = sqlite3.connect("admin.db")
-                conn = connection.cursor()
                 conn.execute("SELECT * FROM subscribers")
                 mydata = conn.fetchall()
                 for char in mydata:
@@ -122,15 +109,13 @@ def post():
                 return render_template("post.html",message="Successfully Posted the data")
             return render_template("post.html",message="")
         return redirect("/login")
-    except:
-        return render_template("post.html",message="Something Went Wrong")
+    except Exception as error:
+        return render_template("post.html",message=f'{error}')
 
 #Deleting a post
 @app.route("/delete",methods=["GET","DELETE"])
 def delete():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
@@ -140,8 +125,6 @@ def delete():
             name = request.args.get("title")
             if not name:
                 return render_template("delete.html",message="* Enter Title")
-            connection = sqlite3.connect("post.db")
-            conn = connection.cursor()
             conn.execute(f'SELECT * FROM post WHERE title="{name}"')
             mydata = conn.fetchall()
             length = len(mydata)
@@ -152,15 +135,13 @@ def delete():
                 connection.commit()
                 return render_template("delete.html",message="Deleted data successfully")
         return render_template("delete.html",message="")
-    except:
-        return render_template("delete.html",message = "Something Went Wrong")
+    except Exception as error:
+        return render_template("delete.html",message = f'{error}')
 
 #Updating a post
 @app.route("/put",methods=["GET","PUT"])
 def put():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
@@ -171,8 +152,6 @@ def put():
             description = request.args.get("description")
             if not name or not description:
                 return render_template("put.html",message="* Enter all fields")
-            connection = sqlite3.connect("post.db")
-            conn = connection.cursor()
             conn.execute(f'SELECT * FROM post WHERE title="{name}"')
             mydata = conn.fetchall()
             length = len(mydata)
@@ -183,14 +162,14 @@ def put():
                 connection.commit()
                 return render_template("put.html",message = "Successfully updated the Post")
         return render_template("put.html",message="")
-    except:
-        return render_template("put.html",message = "Something Went Wrong")
+    except Exception as error:
+        return render_template("put.html",message = f'{error}')
 
 #Logout API
 @app.route("/logout")
 def logout():
     try:
-        connection = sqlite3.connect("admin.db")
+        connection = sqlite3.connect("post.db")
         conn = connection.cursor()
         conn.execute("UPDATE admin SET userlogin='failure'")
         connection.commit()
@@ -202,7 +181,7 @@ def logout():
 @app.route("/createsubscriber")
 def createsubscriber():
     try:
-        connection = sqlite3.connect("admin.db")
+        connection = sqlite3.connect("post.db")
         conn = connection.cursor()
         conn.execute("CREATE TABLE IF NOT EXISTS subscribers(name VARCHAR(40),email VARCHAR(40),phonenumber INTEGER)")
         return "<h1>Successfull<h1>"
@@ -220,9 +199,6 @@ def subscribe():
 
             if not name or not email or not phonenumber:
                 return render_template("subscribe.html",message="* Enter all fields")
-        
-            connection = sqlite3.connect("admin.db")
-            conn = connection.cursor()
     
             msg = Message(
                 'Traveling Blog Notification',
@@ -242,12 +218,10 @@ def subscribe():
 @app.route("/createcomment")
 def createcomment():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("CREATE TABLE IF NOT EXISTS comment(name VARCHAR(40),comment VARCHAR(400))")
         return "<h1>Successfull<h1>"
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #Adding a Comment
 @app.route("/comment",methods = ["GET","POST"])
@@ -259,55 +233,45 @@ def comment():
 
             if not name or not comment:
                 return render_template("comment.html",message="*Enter all fields")
-        
-            connection = sqlite3.connect("admin.db")
-            conn = connection.cursor()
+
             conn.execute(f'INSERT INTO comment VALUES("{name}","{comment}")')
             connection.commit()
             return render_template("comment.html",message="Commented Successfully")
         return render_template("comment.html",message="")
-    except:
-        return render_template("comment.html",message = "Somethoing Went Wrong")
+    except Exception as error:
+        return render_template("comment.html",message = f'{error}')
 
 #Read Comment
 @app.route("/readcomment")
 def readcomment():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM comment")
         mydata = conn.fetchall()
         return render_template("readcomment.html",mydata = mydata)
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 # getting the subscription data
 @app.route("/subscriberslist")
 def subscriberslist():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
         if data == "failure":
             return redirect("/login")
         else:
-            connection = sqlite3.connect("admin.db")
-            conn = connection.cursor()
             conn.execute("SELECT * FROM subscribers")
             mydata = conn.fetchall()
             return render_template("subscriberslist.html",mydata = mydata)
         return render_template("subscriberslist.html",mydata = mydata)
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 #subscribers data delete
 @app.route("/subscriberdelete")
 def subscribersdelete():
     try:
-        connection = sqlite3.connect("admin.db")
-        conn = connection.cursor()
         conn.execute("SELECT * FROM admin")
         mydata = conn.fetchone()
         data = mydata[3]
@@ -317,8 +281,6 @@ def subscribersdelete():
             name = request.args.get("name")
             if not name:
                 return render_template("subscriberdelete.html",message="* Enter Name")
-            connection = sqlite3.connect("admin.db")
-            conn = connection.cursor()
             conn.execute(f'SELECT * FROM subscribers WHERE name="{name}"')
             mydata = conn.fetchall()
             length = len(mydata)
@@ -329,8 +291,8 @@ def subscribersdelete():
                 connection.commit()
                 return render_template("delete.html",message = "Deleted data successfully")
         return render_template("delete.html",message = "")
-    except:
-        return "Something Went Wrong"
+    except Exception as err:
+        return f'{err}'
 
 
 if __name__=="__main__":
